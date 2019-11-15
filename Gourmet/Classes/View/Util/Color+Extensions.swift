b8172708857815e59772d8b2cb6368272b1a381b
Susumu Hoshikawa <shoshikawa@up-frontier.jp>
2019-11-15 14:41:56 +0900

    タグのDark Modeに対応

---
+++ Gourmet/Classes/View/Util/Color+Extensions.swift
@@ -0,0 +1,18 @@
+//
+//  Color+Extensions.swift
+//  Gourmet
+//
+//  Created by shoshikawa on 2019/11/15.
+//  Copyright © 2019 SH Lab, Inc. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    
+    // タグの色.
+    static var tag: Color {
+        return Color("tag")
+    }
+}
